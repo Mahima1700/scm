290f902a4a0b02d7f61e394396ac86ac06cf58f3
Mahima Kumari <126542913+Mahima1700@users.noreply.github.com>
2025-09-22 21:43:51 +0530

    Add files via upload

---
+++ tailwindcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/main/resources/templates/**/*.{html,js}',
+    './src/main/webapp/**/*.jsp',
+    './src/main/resources/static/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode:"selector",
+}
